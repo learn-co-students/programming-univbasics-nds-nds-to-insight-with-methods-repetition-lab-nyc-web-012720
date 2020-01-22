16b4b7cbe64573d3ccce43d8e26cd464ad8e4850
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-22 23:41:52 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -15,7 +15,6 @@ end
 def gross_for_director(d)
   total = 0
   index = 0
-
   while index < d[:movies].length do
     total += d[:movies][index][:worldwide_gross]
     index += 1
@@ -24,13 +23,32 @@ def gross_for_director(d)
   total
 end
 
-def list_of_directors(source)
+def list_of_directors(source) #[{:name=>"Larry"}, {:name=>"Curly"}, {:name=>"Moe"}, {:name=>"Iggy"}]
   # Write this implementation
+  pp source
+  list = []
+  director_index = 0
+  while director_index < source.length do
+    list << source[director_index][:name]
+    director_index += 1
+  end
+  list
 end
 
 def total_gross(source)
+
   # Write this implementation
-  #
+
+  director_earnings_hash = directors_totals(source)
+  list = list_of_directors(source) #creates a variable for array printed by list_of_directors
+  director_index = 0
+  grand_total = 0
+  while director_index < list.length do
+    name = list[director_index]
+    grand_total += director_earnings_hash[name] #this is a hash, how do we access each worldwide_gross to add to grand total?
+    director_index += 1
+  end
+  grand_total
   # Should use methods:
   # 1. directors_totals: returns a Hash of { dir_name => gross }
   # 2. list_of_directors: names provides an Array of directors names (use
@@ -39,5 +57,3 @@ def total_gross(source)
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
 end
-
-
