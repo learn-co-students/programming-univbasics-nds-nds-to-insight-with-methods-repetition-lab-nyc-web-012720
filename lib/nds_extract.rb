3ca103aa055fdfcf7a4eafd1c1eb99c645e0003d
Sam Chen <haihuan.chen@hotmail.com>
2020-01-24 17:52:41 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -25,19 +25,21 @@ def gross_for_director(d)
 end
 
 def list_of_directors(source)
-  # Write this implementation
+  require 'pp'
+  pp directors_database
 end
 
 def total_gross(source)
-  # Write this implementation
-  #
-  # Should use methods:
-  # 1. directors_totals: returns a Hash of { dir_name => gross }
-  # 2. list_of_directors: names provides an Array of directors names (use
-  #
-  # Visit each key (i.e. director name), look up the value in the hash
-  # returned by directors_totals, and add it to a running total. When done,
-  # return the total
+  grand_total = 0
+row_index = 0
+while row_index < vm.length do
+  column_index = 0
+  while column_index < vm[row_index].length do
+    grand_total += total_value_of_spinner(vm, row_index, column_index)
+    column_index += 1
+  end
+  row_index += 1
+end
 end
 
 
